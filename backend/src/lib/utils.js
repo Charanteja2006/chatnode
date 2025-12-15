3ccb1313e4ac1d7120cd22c6c050d70f3787567a
Y L Charan Teja <charantejayandra@gmail.com>
2025-12-15 13:19:09 +0530

    updated some files

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId,res) => {
+    const { JWT_SECRET } = process.env;
+    if(!JWT_SECRET){
+        throw new Error("JWT_SECRET is not defined in environment variables");
+    }
+
+
     const token = jwt.sign({userId: userId}, process.env.JWT_SECRET,{ expiresIn: process.env.JWT_EXPIRES_IN});
 
     res.cookie("jwt",token,{
