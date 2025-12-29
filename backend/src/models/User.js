9e1779a994a205cf1fc66681dd55e1e49fee6394
Y L Charan Teja <charantejayandra@gmail.com>
2025-12-29 23:38:21 +0530

    corrected profilePic field in the db

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema(
             required: true,
             minlength: 6
         },
-        profilrPic:{
+        profilePic:{
             type: String,
             default: "",
         },
