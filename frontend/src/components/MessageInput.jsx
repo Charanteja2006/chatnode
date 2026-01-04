7533b27e2571f2b91f4a175ae4d34fc9e89e0f3a
Y L Charan Teja <charantejayandra@gmail.com>
2026-01-04 23:03:11 +0530

    setup the messageInput component

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,6 +1,55 @@
-import React from 'react'
+import useKeyboardSound from "../hooks/useKeyboardSound";
+import { useChatStore } from "../store/useChatStore";
 
 function MessageInput() {
+  const { playRandomKeyStrokeSound } = useKeyboardSound();
+  const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const fileInputRef = useRef(null);
+  const { sendMesssage, isSoundEnabled } = useChatStore();
+
+  const handleSendMessage = (e) => {
+    e.preventDefault()
+    if(!text.trim() && !imagePreview) return;
+
+    if(isSoundEnabled){
+        playRandomKeyStrokeSound();
+    }
+
+    sendMesssage({
+      text: text.trim(),
+      image: imagePreview,
+    })
+
+    setText("");
+    setImagePreview("");
+    if(fileInputRef.current){
+      fileInputRef.current.value = "";
+    }
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if(!file.type.startsWith("image/")){
+      toast.error("Please select a valid image file");
+      return;
+    }
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+    };
+    reader.readAsDataURL(file);
+
+  };
+
+  const removeImage = () => {
+    setImagePreview(null);
+    if(fileInputRef.current){
+      fileInputRef.current.value = "";
+    }
+  };
+
   return (
     <div>MessageInput</div>
   )
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -56,5 +56,18 @@ export const useChatStore = create((set,get) => ({
         } finally{
             set({ isMessagesLoading:false });
         }
-    }
+    },
+
+    sendMessage: async(messageData) => {
+        const { selectedUser,messages } = get();
+
+        try{
+            const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`,messageData);
+            set({messages: messages.concat(res.data.newMessage)})
+        } catch(error) {
+            toast.error(error.response?.data?.message || "Something went wrong");
+        }
+    },
+
+
 }))
